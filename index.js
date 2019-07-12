d8e75d135a8e8b324ced92a908fa9898bb1887c4
banobepascal <banobepascal@gmail.com>
2019-07-12 11:34:41 +0300

    adding stack methods pop push peek

--- index.js
+++ index.js
@@ -22,3 +22,34 @@ if (rword === word){
 }
 
 // create a stack
+let stack = function() {
+    this.count = 0;
+    this.storage = {};
+
+    // Adds a value onto the end of a stack
+    this.push = function(value){
+        this.storage[this.count] = value;
+        this.count++;
+    }
+
+    // removes and returns the value at the end of the stack
+    this.pop = function() {
+        if(this.count === 0) {
+            return undefined;
+        }
+
+        this.count--;
+        let result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+
+    this.size = function() {
+        return this.count;
+    }
+
+    // Returns the value at the end of the stack
+    this.peek = function(){
+        return this.storage[this.count-1];
+    }
+}
