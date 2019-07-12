b8587dd2f57b6a3924ad525b6c12917c252b3d89
banobepascal <banobepascal@gmail.com>
2019-07-12 18:30:49 +0300

    changing to es6 syntax

--- queues/index.js
+++ queues/index.js
@@ -1,30 +1,30 @@
 
-
-function Queue() {
-    let collection = [];
-    // all items in the queue
-    this.print = function(){
-        console.log(collection);
-    };
-    this.enque = function(element){
-         collection.push(element);
-    };
-    // remove an item from the queue
-    this.denque = function(){
-        return collection.shift();
-    };
-    // getting first element on the queue
-    this.front = function(){
-        return collection[0];
-    };
-    // getting length of the queue
-    this.size = function(){
-        return collection.length;
-    }
-    // getting errors in queue
-    this.isEmpty = function(){
-        return (collection.length === 0)
-
+class Queue {
+    constructor() {
+        let collection = [];
+        // all items in the queue
+        this.print = function () {
+            console.log(collection);
+        };
+        this.enque = function (element) {
+            collection.push(element);
+        };
+        // remove an item from the queue
+        this.denque = function () {
+            return collection.shift();
+        };
+        // getting first element on the queue
+        this.front = function () {
+            return collection[0];
+        };
+        // getting length of the queue
+        this.size = function () {
+            return collection.length;
+        };
+        // getting errors in queue
+        this.isEmpty = function () {
+            return (collection.length === 0);
+        };
     }
 }
 
@@ -40,46 +40,48 @@ console.log(q.front());
 
 
 
-function Priorityqueue() {
-    let collection = [];
-    // all items in the queue
-    this.print = function(){
-        console.log(collection);
-    };
-    this.enque = function(element){
-         if (this.isEmpty()){
-             collection.push(element);
-         } else {
-             let added = false;
-             for (let i=0; i < collection.length; i++){
-                 if (element[1] < collection[i][1]){
-                     collection.splice(i,0,element);
-                     added = true;
-                     break;
-                 }
-             }
-             if (!added){
-                 collection.push(element);
-             }
-         }
-    };
-    // remove an item from the queue
-    this.denque = function(){
-        let value = collection.shift();
-        return value[0];
-    };
-    // getting first element on the queue
-    this.front = function(){
-        return collection[0];
-    };
-    // getting length of the queue
-    this.size = function(){
-        return collection.length;
-    }
-    // getting errors in queue
-    this.isEmpty = function(){
-        return (collection.length === 0)
-
+class Priorityqueue {
+    constructor() {
+        let collection = [];
+        // all items in the queue
+        this.print = function () {
+            console.log(collection);
+        };
+        this.enque = function (element) {
+            if (this.isEmpty()) {
+                collection.push(element);
+            }
+            else {
+                let added = false;
+                for (let i = 0; i < collection.length; i++) {
+                    if (element[1] < collection[i][1]) {
+                        collection.splice(i, 0, element);
+                        added = true;
+                        break;
+                    }
+                }
+                if (!added) {
+                    collection.push(element);
+                }
+            }
+        };
+        // remove an item from the queue
+        this.denque = function () {
+            let value = collection.shift();
+            return value[0];
+        };
+        // getting first element on the queue
+        this.front = function () {
+            return collection[0];
+        };
+        // getting length of the queue
+        this.size = function () {
+            return collection.length;
+        };
+        // getting errors in queue
+        this.isEmpty = function () {
+            return (collection.length === 0);
+        };
     }
 }
 
@@ -89,3 +91,4 @@ pq.enque(['Tonny', 2])
 pq.enque(['Neymar', 1])
 pq.enque(['Messi', 3])
 pq.print();
+console.log(pq.size());
