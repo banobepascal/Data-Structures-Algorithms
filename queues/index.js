f8d1e319533e3388716125099985841d63020b83
banobepascal <banobepascal@gmail.com>
2019-07-12 17:56:21 +0300

    making the queue

---
+++ queues/index.js
@@ -0,0 +1,36 @@
+
+
+function Queue() {
+    let collection = [];
+    // all items in the queue
+    this.print = function(){
+        console.log(collection);
+    };
+    this.enque = function(element){
+         collection.push(element);
+    };
+    // remove an item from the queue
+    this.denque = function(){
+        return collection.shift();
+    };
+    // getting first element on the queue
+    this.front = function(){
+        return collection[0];
+    };
+    // getting length of the queue
+    this.size = function(){
+        return collection.length;
+    }
+    // getting errors in queue
+    this.avail = function(){
+        return (collection.length === 0)
+
+    }
+}
+
+const q = new Queue();
+q.enque('a');
+q.enque('b');
+q.enque('c');
+q.enque('d');
+q.print();
