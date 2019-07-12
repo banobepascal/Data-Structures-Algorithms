57fcc1de7cb80b29cbdb2577d8ffdefc6c4531fa
banobepascal <banobepascal@gmail.com>
2019-07-12 19:03:27 +0300

    adding functionality to BST

--- binary-tree/index.js
+++ binary-tree/index.js
@@ -8,5 +8,26 @@ class Node{
 }
 
 class BST{
-    constructor(){}
+    constructor(){
+        this.root = null;
+    }
+    add(data){
+        const node = this.root;
+        if (node === null){
+            this.root = new Node(data);
+            return;
+        } else {
+            const searchTree = function(node){
+                if (data < node.data){
+                    if (node.left === null){
+                        node.left = new Node(data);
+                        return;
+                    } else if (node.left !== null) {
+                        return searchTree(node.left);
+                    }
+                }
+            }
+        }
+    }
+
 }
