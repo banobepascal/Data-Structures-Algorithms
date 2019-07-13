c6bfcb67755b0df577893e0a51f496bf3d188bf2
banobepascal <banobepascal@gmail.com>
2019-07-13 13:39:28 +0300

    checjing for tree balance

--- binary-tree/index.js
+++ binary-tree/index.js
@@ -116,6 +116,10 @@ class BST {
     };
     this.root = removeNode(this.root, data);
   }
+  isBalanced() {
+    return (this.findMinHeight() >= this.findMaxHeight() -1)
+  }
+  
 }
 
 const bst = new BST();
